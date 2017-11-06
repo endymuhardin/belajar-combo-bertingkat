f666ebe6ad1a26faef67da52b5acb3aa58455bf7
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-06 11:10:05 +0700

    skema database

---
+++ src/main/resources/db/migration/V1.0.0.2017110601__Skema_Awal.sql
@@ -0,0 +1,33 @@
+CREATE TABLE provinsi (
+  id   VARCHAR(36),
+  nama VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (nama)
+);
+
+CREATE TABLE kabupaten_kota (
+  id          VARCHAR(36),
+  id_provinsi VARCHAR(36) NOT NULL,
+  nama VARCHAR (255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (nama),
+  FOREIGN KEY (id_provinsi) REFERENCES provinsi (id)
+);
+
+CREATE TABLE kecamatan (
+  id                VARCHAR(36),
+  id_kabupaten_kota VARCHAR(36)  NOT NULL,
+  nama              VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (nama),
+  FOREIGN KEY (id_kabupaten_kota) REFERENCES kabupaten_kota (id)
+);
+
+CREATE TABLE kelurahan (
+  id           VARCHAR(36),
+  id_kecamatan VARCHAR(36)  NOT NULL,
+  nama         VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (nama),
+  FOREIGN KEY (id_kecamatan) REFERENCES kecamatan (id)
+);
---
+++ src/main/resources/db/migration/V1.0.0.2017110602__Data_Awal.sql
@@ -0,0 +1,22 @@
+INSERT INTO provinsi (id, nama) VALUES ('dki', 'DKI Jakarta');
+INSERT INTO provinsi (id, nama) VALUES ('jatim', 'Jawa Timur');
+INSERT INTO provinsi (id, nama) VALUES ('jabar', 'Jawa Barat');
+
+INSERT INTO kabupaten_kota (id, id_provinsi, nama) VALUES ('kabbogor', 'jabar', 'Kabupaten Bogor');
+INSERT INTO kabupaten_kota (id, id_provinsi, nama) VALUES ('kotabogor', 'jabar', 'Kota Bogor');
+INSERT INTO kabupaten_kota (id, id_provinsi, nama) VALUES ('surabaya', 'jatim', 'Surabaya');
+INSERT INTO kabupaten_kota (id, id_provinsi, nama) VALUES ('mojokerto', 'jatim', 'Mojokerto');
+
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('cibinong', 'kabbogor', 'Cibinong');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('gnputri', 'kabbogor', 'Gunung Putri');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('bootimur', 'kotabogor', 'Bogor Timur');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('boobarat', 'kotabogor', 'Bogor Barat');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('rungkut', 'surabaya', 'Rungkut');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('wonokromo', 'surabaya', 'Wonokromo');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('mojosari', 'mojokerto', 'Mojosari');
+INSERT INTO kecamatan (id, id_kabupaten_kota, nama) VALUES ('trowulan', 'mojokerto', 'Trowulan');
+
+INSERT INTO kelurahan (id, id_kecamatan, nama) VALUES ('tengah', 'cibinong', 'Tengah');
+INSERT INTO kelurahan (id, id_kecamatan, nama) VALUES ('pakansari', 'cibinong', 'Pakansari');
+INSERT INTO kelurahan (id, id_kecamatan, nama) VALUES ('ciangsana', 'gnputri', 'Ciangsana');
+INSERT INTO kelurahan (id, id_kecamatan, nama) VALUES ('cikeas', 'gnputri', 'Cikeas');
