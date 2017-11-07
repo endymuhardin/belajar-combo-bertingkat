4f4548fbf411e4b629cb102e3d0e56d33ff1e98e
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-07 14:30:48 +0700

    implement combo bertingkat dengan jquery

--- src/main/resources/static/js/aplikasi.js
+++ src/main/resources/static/js/aplikasi.js
@@ -1,18 +1,96 @@
-window.addEventListener('load', function () {
-
+$(document).ready(function(){
     var urlProvinsi = "/api/provinsi";
-    var aplikasi = new Vue({
-        el: '#aplikasi-registrasi',
-        components: {
-            autocomplete: Vue2Autocomplete.default
-        },
-        data: {
-            provinsi: null
-        },
-        methods: {
-            selectProvinsi: function(prov){
-                console.log(prov);
-            }
+
+    var templateUrlKabupaten = "/api/provinsi/{provinsi}/kabupaten";
+    var templateUrlKecamatan = "/api/kota/{kabupatenkota}/kecamatan";
+    var templateUrlKelurahan = "/api/kecamatan/{kecamatan}/kelurahan";
+
+    var urlKabupaten = null;
+    var urlKecamatan = null;
+    var urlKelurahan = null;
+
+    var provinsi = null;
+    var kabupatenKota = null;
+    var kecamatan = null;
+    var kelurahan = null;
+
+    var inputProvinsi = $("#provinsi");
+    var inputKabupatenKota = $("#kabupatenKota");
+    var inputKecamatan = $("#kecamatan");
+    var inputKelurahan = $("#kelurahan");
+    var inputKodepos = $("#kodepos");
+
+    var resetInput = function(inputField){
+        inputField.val('');
+        inputField.prop('disabled', true);
+    };
+
+    resetInput(inputKabupatenKota);
+    resetInput(inputKecamatan);
+    resetInput(inputKelurahan);
+
+    inputProvinsi.typeahead({
+        displayText: function(item){ return item.nama;},
+        source: _.debounce(function(cari, process){
+            provinsi = null;
+            resetInput(inputKabupatenKota);
+            resetInput(inputKecamatan);
+            resetInput(inputKelurahan);
+            $.get(urlProvinsi, {nama: cari}, function(hasil){
+                process(hasil);
+            }, "json");
+        }, 500),
+        afterSelect: function(pilihan){
+            provinsi = pilihan;
+            inputKabupatenKota.prop('disabled', false);
+            urlKabupaten = _.replace(templateUrlKabupaten, '{provinsi}', provinsi.id);
+        }
+    });
+
+    inputKabupatenKota.typeahead({
+        displayText: function(item){ return item.nama;},
+        source: _.debounce(function(cari, process){
+            kabupatenKota = null;
+            resetInput(inputKecamatan);
+            resetInput(inputKelurahan);
+            $.get(urlKabupaten, {nama: cari}, function(hasil){
+                process(hasil);
+            }, "json");
+        }, 500),
+        afterSelect: function(pilihan){
+            kabupatenKota = pilihan;
+            inputKecamatan.prop('disabled', false);
+            urlKecamatan = _.replace(templateUrlKecamatan, '{kabupatenkota}', kabupatenKota.id);
+        }
+    });
+
+    inputKecamatan.typeahead({
+        displayText: function(item){ return item.nama;},
+        source: _.debounce(function(cari, process){
+            kecamatan = null;
+            resetInput(inputKelurahan);
+            $.get(urlKecamatan, {nama: cari}, function(hasil){
+                process(hasil);
+            }, "json");
+        }, 500),
+        afterSelect: function(pilihan){
+            kecamatan = pilihan;
+            inputKelurahan.prop('disabled', false);
+            urlKelurahan = _.replace(templateUrlKelurahan, '{kecamatan}', kecamatan.id);
+        }
+    });
+
+    inputKelurahan.typeahead({
+        displayText: function(item){ return item.nama;},
+        source: _.debounce(function(cari, process){
+            kecamatan = null;
+            $.get(urlKelurahan, {nama: cari}, function(hasil){
+                process(hasil);
+            }, "json");
+        }, 500),
+        afterSelect: function(pilihan){
+            kelurahan = pilihan;
+            inputKodepos.val(kelurahan.kodepos);
         }
     });
 });
