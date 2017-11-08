a681ca3d14cdda0af53f0f6cd2901bbec6e02184
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-08 07:53:46 +0700

    redirect homepage

--- src/main/java/com/muhardin/endy/belajar/combobertingkat/controller/RegistrasiController.java
+++ src/main/java/com/muhardin/endy/belajar/combobertingkat/controller/RegistrasiController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,11 @@ public class RegistrasiController {
     @InitBinder
     protected void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(Kelurahan.class, kelurahanEditor);
+    }
+
+    @GetMapping("/")
+    public String homepage(){
+        return "redirect:/registrasi.html";
     }
 
     @PostMapping("/registrasi")
